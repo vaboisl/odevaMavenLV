a5dd14415ce18f8a343368588f13339b588c73e1
Vabois Loick <vabois@a14p10.fil.univ-lille1.fr>
2018-10-26 09:09:53 +0200

    commit initial : Travis config + Projet mvn pour inte cont

---
+++ src/main/java/myapp/dev/App.java
@@ -0,0 +1,13 @@
+package myapp.dev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
